353813c026c0edee82e4c30c98682fdfd21681bd
Will Thomas <30wthomas@gmail.com>
2023-03-07 09:43:12 -0600

    Simplifying sets, added issue for CSS later

--- frontEnd/src/components/Sets/Sets.tsx
+++ frontEnd/src/components/Sets/Sets.tsx
@@ -3,40 +3,52 @@ import { CardData, Flashcard } from "../Flashcard/Flashcard";
 import { Slideshow } from "../Slideshow/Slideshow";
 import "./Sets.css";
 
-const SETS: CardData[] = [
-  {
-    front: "front1",
-    back: "back1",
-    flipped: false,
-    setFlipped: (b: boolean) => (SETS[0].flipped = b),
-  },
-  {
-    front: "front2",
-    back: "back2",
-    flipped: false,
-    setFlipped: (b: boolean) => (SETS[1].flipped = b),
-  },
-  {
-    front: "front3",
-    back: "back3",
-    flipped: false,
-    setFlipped: (b: boolean) => (SETS[2].flipped = b),
-  },
-  {
-    front: "front4",
-    back: "back4",
-    flipped: false,
-    setFlipped: (b: boolean) => (SETS[3].flipped = b),
-  },
-];
+// const SETS: CardData[] = ;
 
 export const Sets = () => {
-  // const [cards, setCards] = React.useState<CardData[]>([]);
+  const [cards, setCards] = React.useState<CardData[]>([
+    {
+      front: "front1",
+      back: "back1",
+      flipped: false,
+      setFlipped: (b: boolean) => {
+        const microView = cards.filter((_, ind) => ind !== 0);
+        setCards([{ ...cards[0], flipped: b }, ...microView]);
+      },
+    },
+    {
+      front: "front2",
+      back: "back2",
+      flipped: false,
+      setFlipped: (b: boolean) => {
+        const microView = cards.filter((_, ind) => ind !== 1);
+        setCards([{ ...cards[1], flipped: b }, ...microView]);
+      },
+    },
+    {
+      front: "front3",
+      back: "back3",
+      flipped: false,
+      setFlipped: (b: boolean) => {
+        const microView = cards.filter((_, ind) => ind !== 2);
+        setCards([{ ...cards[2], flipped: b }, ...microView]);
+      },
+    },
+    {
+      front: "front4",
+      back: "back4",
+      flipped: false,
+      setFlipped: (b: boolean) => {
+        const microView = cards.filter((_, ind) => ind !== 3);
+        setCards([{ ...cards[3], flipped: b }, ...microView]);
+      },
+    },
+  ]);
 
   return (
     <div className="sets-container">
       <div className="sets-slideshow">
-        <Slideshow Component={Flashcard} data={SETS} />
+        <Slideshow Component={Flashcard} data={cards} />
       </div>
     </div>
   );
--- frontEnd/src/components/Slideshow/Slideshow.tsx
+++ frontEnd/src/components/Slideshow/Slideshow.tsx
@@ -13,134 +13,32 @@ export function Slideshow<T>({
   data: T[];
 }): JSX.Element {
   const [currentIndex, setCurrentIndex] = React.useState(0);
-  const [viewingSlice, setViewingSlice] = React.useState<T[]>([]);
-
-  React.useEffect(() => {
-    const gatherSlice = (data: T[], ind: number): T[] => {
-      const len = data.length;
-      const indices = [
-        (ind - 2 + len) % len,
-        (ind - 1 + len) % len,
-        ind,
-        (ind + 1) % len,
-        (ind + 2) % len,
-      ];
-      return indices.map((ind) => data[ind]);
-    };
-
-    setViewingSlice(gatherSlice(data, currentIndex));
-  }, [data, currentIndex]);
-
-  const add_classes = (
-    elemSelect: string,
-    classList: string[],
-    delay: number
-  ) => {
-    const elem = document.querySelector(elemSelect);
-    if (elem) {
-      classList.forEach((className) => {
-        elem.classList.add(className);
-        setTimeout(() => {
-          elem.classList.remove(className);
-        }, delay);
-      });
-    } else {
-      console.error("Could not find", elemSelect);
-    }
-  };
 
   const prevItem = () => {
-    add_classes(".slideshow-item-left", ["shift-left"], 200);
-    add_classes(".slideshow-item-active", ["shift-left"], 200);
-    add_classes(".slideshow-item-right", ["shift-left"], 200);
-    if (currentIndex === 0) {
-      setCurrentIndex(data.length - 1);
-    } else {
-      setCurrentIndex(currentIndex - 1);
-    }
+    setCurrentIndex((currentIndex - 1 + data.length) % data.length);
   };
 
   const nextItem = () => {
-    add_classes(
-      ".slideshow-item-left",
-      ["shift-right", "opacity-100", "basis-50"],
-      2000
-    );
-    add_classes(
-      ".slideshow-item-active",
-      ["shift-right", "opacity-40", "basis-25"],
-      2000
-    );
-    add_classes(
-      ".slideshow-item-right",
-      ["shift-right", "opacity-0", "basis-0"],
-      2000
-    );
-  };
-
-  const retrieve_config = (ind: number) => {
-    switch (ind) {
-      case 0:
-        return {
-          className: "slideshow-item-left-left",
-          pos: "left-left",
-          onClick: () => {},
-          transEnd: () => {},
-        };
-      case 1:
-        return {
-          className: "slideshow-item-left",
-          pos: "left",
-          onClick: prevItem,
-          transEnd: () => {
-            setCurrentIndex((currentIndex + 1 + data.length) % data.length);
-          },
-        };
-      case 2:
-        return {
-          className: "slideshow-item-active",
-          pos: "focused",
-          onClick: () => {},
-          transEnd: () => {},
-        };
-      case 3:
-        return {
-          className: "slideshow-item-right",
-          pos: "right",
-          onClick: nextItem,
-          transEnd: () => {
-            setCurrentIndex((currentIndex + 1 + data.length) % data.length);
-          },
-        };
-      case 4:
-        return {
-          className: "slideshow-item-right-right",
-          pos: "right-right",
-          onClick: () => {},
-          transEnd: () => {},
-        };
-      default:
-        throw new Error("Invalid index for slideshow item");
-    }
+    setCurrentIndex((currentIndex + 1) % data.length);
   };
 
   return (
     <div className="slideshow-container">
-      {viewingSlice.map((val, ind) => {
-        const { className, pos, onClick, transEnd } = retrieve_config(ind);
+      {/* {viewingSlice.map((val, ind) => {
+        const conf = retrieve_config(ind);
         return (
           <div
             key={`slideshow-item-ind-${ind}`}
-            className={className}
-            onClick={onClick}
-            onTransitionEnd={transEnd}
+            className={conf.className}
+            onClick={miniShiftLeft}
+            // onTransitionEnd={transEnd}
           >
-            <Component data={val} position={pos} />
+            <Component data={val} position={conf.pos} />
           </div>
         );
-      })}
+      })} */}
 
-      {/* <div className="slideshow-item-left" onClick={prevItem}>
+      <div className="slideshow-item-left" onClick={prevItem}>
         <Component
           data={data[(currentIndex - 1 + data.length) % data.length]}
           position={"left"}
@@ -154,7 +52,7 @@ export function Slideshow<T>({
           data={data[(currentIndex + 1) % data.length]}
           position={"right"}
         />
-      </div> */}
+      </div>
     </div>
   );
 }
